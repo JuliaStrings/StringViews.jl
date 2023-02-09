54f9f668b5af0fed5ab8a39a2efb7f866183ffd4
Steven G. Johnson <stevenj@alum.mit.edu>
2023-02-09 18:51:12 -0500

    more docs for SVRegexMatch

--- src/regex.jl
+++ src/regex.jl
@@ -120,10 +120,26 @@ end
 #####################################################################
 # need to duplicate this code from Base because of julia#48617:
 """
-    SVRegexMatch
+    SVRegexMatch <: AbstractMatch
 
-This type is identical to `Base.SVRegexMatch` except that the
+This type is identical to `RegexMatch` (in Julia `Base`) except that the
 `match` is a `SubString` of a `StringView` instead of a `String`.
+
+A type representing a single match to a `Regex` found in a string.
+Typically created from the [`match`](@ref) function.
+
+* The `match` field stores the substring of the entire matched string.
+* The `captures` field stores the substrings for each capture group, indexed by number.
+  To index by capture group name, the entire match object should be indexed instead,
+  as shown in the examples.
+* The location of the start of the match is stored in the `offset` field.
+* The `offsets` field stores the locations of the start of each capture group,
+  with 0 denoting a group that was not captured.
+
+This type can be used as an iterator over the capture groups of the `Regex`,
+yielding the substrings captured in each group.
+Because of this, the captures of a match can be destructured.
+If a group was not captured, `nothing` will be yielded instead of a substring.
 """
 struct SVRegexMatch{T<:DenseStringView} <: AbstractMatch
     match::SubString{T}
