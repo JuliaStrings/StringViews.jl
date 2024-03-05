1a22be907d128f60acccdf59939a8817f580abec
Sam Atman <atmanistan@gmail.com>
2024-03-04 14:32:43 -1000

    Update runtests.jl
    And a test for identity equality

--- test/runtests.jl
+++ test/runtests.jl
@@ -158,6 +158,8 @@ end
     @test Base.typemin(s) isa StringView{Vector{UInt8}}
     @test Base.typemin(s) == ""
 
+    @test Base.one(s) == Base.typemin(s) == ""
+
     @test isascii(s)
     @test !isascii(StringView("fööbār"))
 
