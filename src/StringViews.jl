20692e11186a557d168b109a0ca4e231f7bce590
Steven G. Johnson <stevenj@alum.mit.edu>
2026-06-10 15:17:16 -0400

    Update StringViews.jl: rm obsolete IOBuffer constructor docs

--- src/StringViews.jl
+++ src/StringViews.jl
@@ -30,12 +30,6 @@ export StringView, SVRegexMatch
 `StringView(array)` creates an `AbstractString` representation of
 any `array` of `UInt8` data, interpreted as UTF-8 encoded Unicode.
 It does *not* make a copy of or modify `array`.
-
-`StringView(buf::IOBuffer)` returns a string view of the
-current contents of the `buf`, equivalent to `String(take!(buf))`
-but without making a copy.   `StringView(buf::IOBuffer, range)`
-is a view of the bytes `range` (defaults to `1:position(buf)-1`)
-in the buffer.
 """
 struct StringView{T<:AbstractVector{UInt8}} <: AbstractString
     data::T
